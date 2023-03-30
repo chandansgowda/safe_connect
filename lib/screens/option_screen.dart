bf2b286f3cb649d62141cbc6479ae6c9c3ee1ec1
AKASHANGADII <akashakku2202@gmail.com>
2023-03-30 22:46:11 +0530

    Options screen and predicted diseases screen created(renamed DiseasesOptionsScreen)

---
+++ lib/screens/option_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:safe_connect/screens/diseases_options_screen.dart';
+import 'package:safe_connect/screens/questions_screen.dart';
+
+class OptionScreen extends StatelessWidget {
+  const OptionScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("title"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ContainerButton(onPressed: (){
+              Navigator.pushNamed(context, QuestionsScreen.routeName);
+            },text: "Take Quiz",),
+            SizedBox(
+              height: 50,
+            ),
+            Text("OR"),
+            SizedBox(
+              height: 50,
+            ),
+            ContainerButton(text: "Select Disease", onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context)=>DiseasesOptionsScreen()));
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerButton extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+
+  const ContainerButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(14.0),
+          child: Center(child: Text(text)),
+        ),
+      ),
+    );
+  }
+}
