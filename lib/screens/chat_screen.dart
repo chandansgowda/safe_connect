a1653c4826edb263259170bbcf3dc8ca0b768521
chandansgowda <chandansuresh007@gmail.com>
2023-03-30 20:56:22 +0530

    started chat screen implementation

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Room Name"),
+
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:safe_connect/screens/chat_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -18,6 +19,9 @@ class HomeScreen extends StatelessWidget {
           }, icon: Icon(Icons.exit_to_app_outlined))
         ],
       ),
+      floatingActionButton: FloatingActionButton(onPressed: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (_)=> ChatScreen()));
+      }, child: Icon(Icons.message),),
     );
   }
 }
