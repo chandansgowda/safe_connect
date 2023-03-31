b671203ba665439ea699da1859fc1d56f40856f8
chandansgowda <chandansuresh007@gmail.com>
2023-03-31 20:02:19 +0530

    resolved register logic error

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -33,21 +33,22 @@ class SignInPage extends StatelessWidget {
           Map<String, dynamic>? data;
           if(userData.exists) {
             data = userData.data();
-            if(data!.isEmpty){
+            if (data!.isEmpty) {
               await FirebaseFirestore.instance.collection("users").doc(
                   signedIn.user!.email).set({
                 "isDiseaseKnown": false,
                 "disease1": "",
                 "disease2": "",
                 "disease3": "",
-                "dp1":0,
-                "dp2":0,
-                "dp3":0,
+                "dp1": 0,
+                "dp2": 0,
+                "dp3": 0,
                 "joinedGroups": []
               });
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>OptionScreen()));
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => OptionScreen()));
             }
-            else if (data!['disease1']!="") {
+            else if (data!['disease1'] != "") {
               var response = {
                 "disease": [
                   {'name': data['disease1'], 'percent': data['dp1']},
@@ -58,6 +59,7 @@ class SignInPage extends StatelessWidget {
               Navigator.pushNamed(context, PredictedDiseasesScreen.routeName,
                   arguments: response);
             }
+          }
           else{
             await FirebaseFirestore.instance.collection("users").doc(signedIn.user!.email).set({
               "isDiseaseKnown":false,
@@ -69,10 +71,9 @@ class SignInPage extends StatelessWidget {
               "dp3":0,
               "joinedGroups": []
             });
-            //TODO: Send him to select option (quiz/self certify) screen
             Navigator.of(context).push(MaterialPageRoute(builder: (_)=> OptionScreen()));
           }
-        }}),
+        }),
       ],
     );
   }
